3a27cc0f798af6711f3334922bee81d9d8bb17d0
diewland <diewland@gmail.com>
2023-07-30 00:52:20 +0700

    dev gen_json script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # alice-and-bob
+
+* verify contract config
+* mint new nft via etherscan (data = 0x)
---
+++ json/gen_json.py
@@ -0,0 +1,42 @@
+import csv, json
+from pprint import pprint as pp
+
+CSV_PATH    = "./alice-and-bob.csv"
+JSON_PATH   = "../../alice-and-bob-assets/json/{}.json"
+IMG_PATH    = "https://diewland.github.io/alice-and-bob-assets/assets/GNS_{:02}_50.png"
+ENGINE      = "Jigsaw Engine"
+METADATA    = {
+  "name"        : "***",
+  "description" : "***",
+  "image"       : "***",
+  "attributes"  : "***",
+  "compiler"    : ENGINE,
+}
+
+TRAITS = [
+    "Gen",
+    "Class",
+    "Type",
+    "Accessory",
+    "Event",
+    "Gang",
+]
+
+with open(CSV_PATH) as csvfile:
+    rows = csv.DictReader(csvfile)
+    for row in rows:
+        id = int(row["ID"])
+
+        # craft attributes
+        attrs = [] # TODO
+
+        METADATA["name"]        = row["Name"]
+        METADATA["description"] = row["Desc"]
+        METADATA["image"]       = IMG_PATH.format(id)
+        METADATA["attributes"]  = attrs
+
+        # write json file
+        outpath = JSON_PATH.format(id)
+        with open(outpath, "w") as f:
+            json.dump(METADATA, f)
+            print(outpath)
