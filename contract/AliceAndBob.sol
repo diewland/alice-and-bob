3ff9fc2e13af6bca6251b0ce0fbe83363c3988bd
diewland <diewland@gmail.com>
2023-09-21 23:15:27 +0700

    remove operation filter

--- contract/AliceAndBob.sol
+++ contract/AliceAndBob.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import {DefaultOperatorFilterer} from "operator-filter-registry/src/DefaultOperatorFilterer.sol";
 
-contract AliceAndBob is ERC1155PresetMinterPauser, DefaultOperatorFilterer {
+contract AliceAndBob is ERC1155PresetMinterPauser {
 
     string public name = "Alice and Bob";
     string public symbol = "ANB";
@@ -46,27 +45,6 @@ contract AliceAndBob is ERC1155PresetMinterPauser, DefaultOperatorFilterer {
     }
     function tokenURI(uint tokenId) public view returns (string memory) {
         return string.concat(baseURI, Strings.toString(tokenId), ".json");
-    }
-
-    // https://github.com/ProjectOpenSea/operator-filter-registry/blob/main/src/example/ExampleERC1155.sol
-    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
-        super.setApprovalForAll(operator, approved);
-    }
-    function safeTransferFrom(address from, address to, uint256 tokenId, uint256 amount, bytes memory data)
-        public
-        override
-        onlyAllowedOperator(from)
-    {
-        super.safeTransferFrom(from, to, tokenId, amount, data);
-    }
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public virtual override onlyAllowedOperator(from) {
-        super.safeBatchTransferFrom(from, to, ids, amounts, data);
     }
 
 }
